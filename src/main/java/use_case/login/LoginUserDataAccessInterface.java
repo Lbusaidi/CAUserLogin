e72e7e4c75e230ab5cb116a468e5fcba8f2fc74f
lamee <lamees.albusaidi@mail.utoronto.ca>
2024-10-20 22:53:31 -0400

    Task 2.2

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,5 +27,15 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the currently logged-in user.
+     * @param name the username of the current user
+     */
     void setCurrentUser(String name);
+
+    /**
+     * Retrieves the username of the currently logged-in user.
+     * @return the username of the current user
+     */
+    String getCurrentUser();
 }
